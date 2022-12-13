595174fa8f83733b9188b3115d624e486f108e7c
Emil arrenius <Nils-Emil1337@hotmail.se>
2022-12-13 15:09:02 +0100

    ..

---
+++ HotelAppKyh/Controllers/ListGuests.cs
@@ -0,0 +1,37 @@
+﻿using HotelAppKyh.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelAppKyh.Controllers
+{
+    internal class ListGuests
+    {
+        public AppDbContext myContext { get; set; }
+        public ListGuests(AppDbContext context)
+        {
+            myContext = context;
+        }
+
+        public void run()
+        {
+            Console.Clear();
+            Console.WriteLine(" Bookings information");
+            Console.WriteLine("Id\tFname\t\tLname\t\tPhone");
+            Console.WriteLine("================================================");
+            foreach (var guest in myContext.Guests.OrderBy(x => x.GuestId))
+            {
+
+                Console.WriteLine($"{guest.GuestId}\t{guest.FirstName}\t\t{guest.LastName}\t{guest.PhoneNumber}");
+
+            }
+
+            Console.WriteLine("--------------------------------------------------");
+
+            Console.WriteLine("Tryck enter för att fortsätta");
+            Console.ReadLine();
+        }
+    }
+}
