8eac0aaf113e948e8ab9f3f373343a21f6513bd5
Emil arrenius <Nils-Emil1337@hotmail.se>
2023-01-02 13:21:09 +0100

    final

--- HotelAppKyh/Data/DataSeeder.cs
+++ HotelAppKyh/Data/DataSeeder.cs
@@ -96,6 +96,9 @@ namespace HotelAppKyh.Data
                 });
             }
         }
+
+       
+        
     }
 }
 
--- HotelAppKyh/Main.cs
+++ HotelAppKyh/Main.cs
@@ -17,91 +17,101 @@ public class Main
             var delete = new Delete(myContext);
 
 
-            ////
+            
 
 
             while (true)
             {
-                var inuput = MainMenu.ShowMenu();
-
-                if (inuput == 1)
+                try
                 {
-                    create.AddGuest();
+                    var inuput = MainMenu.ShowMenu();
+
+                    if (inuput == 1)
+                    {
+                        create.AddGuest();
+                    }
+
+                    else if (inuput == 2)
+                    {
+                        create.AddRoom();
+                    }
+
+                    else if (inuput == 3)
+                    {
+                        read.ListGuest();
+                        Console.WriteLine("\nTryck enter för att fortsätta");
+                        Console.ReadLine();
+                    }
+
+
+                    else if (inuput == 4)
+                    {
+                        read.ListRoom();
+                        Console.WriteLine("\nTryck enter för att fortsätta");
+                        Console.ReadLine();
+                    }
+
+
+                    else if (inuput == 5)
+                    {
+                        uppdate.UpdateGuest();
+                    }
+
+                    else if (inuput == 6)
+                    {
+                        uppdate.UpdateRoom();
+                    }
+                    else if (inuput == 7)
+                    {
+                        uppdate.AddBed();
+                    }
+
+                    else if (inuput == 8)
+                    {
+                        delete.DeleteGuest();
+                    }
+
+                    else if (inuput == 9)
+                    {
+                        delete.DeleteRoom();
+                    }
+
+                    else if (inuput == 10)
+                    {
+                        create.CreateReservation();
+                    }
+                    else if (inuput == 11)
+                    {
+                        delete.CanselReservation();
+                    }
+                    else if (inuput == 12)
+                    {
+                        uppdate.UpdateReservation();
+                    }
+                    else if (inuput == 13)
+                    {
+                        uppdate.RemoveBed();
+                    }
+                    else if (inuput == 14)
+                    {
+                        read.ListReservations();
+                        Console.WriteLine("\nTryck enter för att fortsätta");
+                        Console.ReadLine();
+                    }
+
+                    else if (inuput == 0)
+                    {
+                        break;
+                    }
                 }
-
-                else if (inuput == 2)
+                catch (Exception e)
                 {
-                    create.AddRoom();
-                }
-
-                else if (inuput == 3)
-                {
-                    read.ListGuest();
-                    Console.WriteLine("\nTryck enter för att fortsätta");
+                    Console.WriteLine(e);
+                    Console.WriteLine("Tryck enter för att fortsäta");
                     Console.ReadLine();
                 }
-
-
-                else if (inuput == 4)
-                {
-                    read.ListRoom();
-                    Console.WriteLine("\nTryck enter för att fortsätta");
-                    Console.ReadLine();
                 }
-
-
-                else if (inuput == 5)
-                {
-                    uppdate.UpdateGuest();
-                }
-
-                else if (inuput == 6)
-                {
-                    uppdate.UpdateRoom();
-                }
-                else if (inuput == 7)
-                {
-                    uppdate.AddBed();
-                }
-
-                else if (inuput == 8)
-                {
-                    delete.DeleteGuest();
-                }
-
-                else if (inuput == 9)
-                {
-                    delete.DeleteRoom();
-                }
-
-                else if (inuput == 10)
-                {
-                    create.CreateReservation();
-                }
-                else if (inuput == 11)
-                {
-                    delete.CanselReservation();
-                }
-                else if (inuput == 12)
-                {
-                    uppdate.UpdateReservation();
-                }
-                else if (inuput == 13)
-                {
-                    uppdate.RemoveBed();
-                }
-                else if (inuput == 14)
-                {
-                    read.ListReservations();
-                    Console.WriteLine("\nTryck enter för att fortsätta");
-                    Console.ReadLine();
-                }
-
-                else if (inuput == 0)
-                {
-                    break;
-                }
-            }
+                
         }
         catch (Exception e)
         {
